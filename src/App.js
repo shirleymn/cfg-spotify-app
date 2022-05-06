851afdeec9c44b6a173fcaaf78a7a965937eb07f
Shirley <shirleyman27@gmail.com>
2022-05-06 19:27:15 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import {useEffect, useState, setToken} from 'react';
+
+
+function App() {
+  const CLIENT_ID = "e5caca910c8b4916ad947b87dc2f36e2"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+      const hash = window.location.hash
+      let token = window.localStorage.getItem("token")
+
+      if (!token && hash) {
+          token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+          window.location.hash = ""
+          window.localStorage.setItem("token", token)
+      }
+
+      setToken(token)
+
+  }, [])
+
+  
+
+  return (
+      <div className="App">
+          <header className="App-header">
+          
+              <h1>Spotify React</h1>
+                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                      to Spotify</a>
+            </header>
+        </div>
+
+    
+  );
+
+  
+}
+
+export default App;
