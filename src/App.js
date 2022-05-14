431606647c201368683e96f048bdb2fd993f64d3
Shirley <shirleyman27@gmail.com>
2022-05-14 02:41:15 +0100

    restructure app.js, add authorise.js, add logged in message

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
 import './App.css';
-import {useEffect, useState, setToken} from 'react';
 
-import Navbar from './components/NavigationBar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
+import Authorise from './pages/authorise';
 import Home from './pages/home';
 import Quiz from './pages/quiz';
 import TopArtists from './pages/topartists';
 import TopTracks from './pages/toptracks';
 
 function App() {
-  // REDIRECT_URI will change when we set up a home page  
-  const CLIENT_ID = "e5caca910c8b4916ad947b87dc2f36e2"
-  const REDIRECT_URI = "http://localhost:3000/home"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-
-  // scopes required to get info from user
-  const SPACE_DELIMITER = "%20";
-  const SCOPES = ["user-top-read",];
-  const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
-
-/*
-http://localhost:3000/#access_token=BQAZOiNXoNjZdgE03LLGQlpU1XiJwqm9YHKE6sgp_2Omj9fLXy2ylyHC1PnSWtRYtucXsX6Qy7sJLEvrICeaEntR_NZh5OSeOvJhyc54CzrgoD2k12nrvx5WiZiW8IFP2l3D4L3tnOb9WziJufc2EdtSPG3jJdzG&token_type=Bearer&expires_in=3600
-example spotify parameters after logging in which generates the user's access token to be used for making requests to API
-*/
-
-// getting access token from URL
-  const getReturnedParametersFromSpotifyAuth = (hash) => {
-    const stringAfterHashtag = hash.substring(1);
-    const parametersInUrl = stringAfterHashtag.split("&");
-    const parametersSplit = parametersInUrl.reduce((accumulater, currentValue) => {
-      console.log(currentValue);
-      const [key, value] = currentValue.split("=");
-      accumulater[key] = value;
-      return accumulater;
-    }, {});
   
-    return parametersSplit;
-  };
-
-    useEffect(() => {
-      if (window.location.hash) {
-        const { access_token, expires_in, token_type } =
-          getReturnedParametersFromSpotifyAuth(window.location.hash);
-  
-        localStorage.clear();
-  
-        localStorage.setItem("accessToken", access_token);
-        localStorage.setItem("tokenType", token_type);
-        localStorage.setItem("expiresIn", expires_in);
-      }
-    });
-
   return (
       <div className="App">
           <header className="App-header">
           
-              <h1>Spotify React</h1>
-                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`}>Login
-                      to Spotify</a>
-
+              <h1>Top Spot</h1>
+                      <Authorise />
                       <Router>
                       <Navbar />
                       <Routes>
---
+++ src/pages/authorise.js
@@ -0,0 +1,70 @@
+// import './Authorise.css';
+import {useEffect, useState, setToken} from 'react';
+
+function Authorise() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // REDIRECT_URI will change when we set up a home page  
+  const CLIENT_ID = "e5caca910c8b4916ad947b87dc2f36e2"
+  const REDIRECT_URI = "http://localhost:3000/home"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+
+  // scopes required to get info from user
+  const SPACE_DELIMITER = "%20";
+  const SCOPES = ["user-top-read",];
+  const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
+/*
+http://localhost:3000/#access_token=BQAZOiNXoNjZdgE03LLGQlpU1XiJwqm9YHKE6sgp_2Omj9fLXy2ylyHC1PnSWtRYtucXsX6Qy7sJLEvrICeaEntR_NZh5OSeOvJhyc54CzrgoD2k12nrvx5WiZiW8IFP2l3D4L3tnOb9WziJufc2EdtSPG3jJdzG&token_type=Bearer&expires_in=3600
+example spotify parameters after logging in which generates the user's access token to be used for making requests to API
+*/
+
+// getting access token from URL
+  const getReturnedParametersFromSpotifyAuth = (hash) => {
+    const stringAfterHashtag = hash.substring(1);
+    const parametersInUrl = stringAfterHashtag.split("&");
+    const parametersSplit = parametersInUrl.reduce((accumulater, currentValue) => {
+      console.log(currentValue);
+      const [key, value] = currentValue.split("=");
+      accumulater[key] = value;
+      return accumulater;
+    }, {});
+  
+    return parametersSplit;
+  };
+
+    useEffect(() => {
+      if (window.location.hash) {
+        const { access_token, expires_in, token_type } =
+          getReturnedParametersFromSpotifyAuth(window.location.hash);
+  
+        localStorage.clear();
+  
+        localStorage.setItem("accessToken", access_token);
+        localStorage.setItem("tokenType", token_type);
+        localStorage.setItem("expiresIn", expires_in);
+        setLoggedIn(true)
+      }
+    });
+
+  return (
+      <div className="Authorise">
+            <header className="Authorise-header">
+            { !loggedIn ?
+                  <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`}>Login
+                      to Spotify</a>
+                      : 
+                        <h3>logged in</h3>
+            }
+            </header>
+            
+        </div>
+
+    
+  );
+}
+  
+
+
+export default Authorise;
