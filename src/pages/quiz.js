905cc9ef2e02294e5a283cdf7ddd3de7ddbc2d66
manal adam <manal@manals-laptop.home>
2022-05-20 20:28:04 +0100

    quiz update

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -7,8 +7,17 @@ const Quiz = () => {
   return (
     <div>
       <h1>Quiz</h1> 
+      <p>Discover how in tune you are with your music history</p>
+      <p>Question 1 of 2</p>
+      <p> What was your top track in the last 4 weeks? </p>
       {/* <QuizLogic /> */} 
       <QuizTopTracksData />
+      <br/>
+      {/* <p>Question 2 of 2</p>
+      <p> Who was your top artist in the last 4 weeks? </p> */}
+ 
+    
+
     </div>
     
   );
